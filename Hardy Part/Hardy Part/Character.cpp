62fd03a2475d185368b1e41821a42ec6d928e3a1
Pawel Stolarz <pabu4444@wp.pl>
2016-04-17 14:46:13 +0200

    Name Update v1 - changing all the instanec of Project1 to Hardy Part

--- Hardy Part/Hardy Part/Character.cpp
+++ Hardy Part/Hardy Part/Character.cpp
@@ -22,10 +22,10 @@ Character * Character::Add(State* stt, std::string name, int x, int y, unsigned 
 		std::cerr << "ERR Character::Add : No state supplied\n";
 		return nullptr;
 	}
-	auto ent = stt->Add_Entity<Character>(layer);
 
 	if (name == "Nerk")
 	{
+		auto ent = stt->Add_Entity<Character>(layer);
 		Sprite::Create(ent, Texture::Load("imgs/nerk-sheet.png", 144, 24, 24, 24, 0, 1));
 		Animation::Add(ent, "idle", "0x80 1x5", true);
 		Animation::Add(ent, "move", "0x4 2-5x4", false);
@@ -35,6 +35,7 @@ Character * Character::Add(State* stt, std::string name, int x, int y, unsigned 
 
 	else if (name == "Dreg")
 	{
+		auto ent = stt->Add_Entity<Character>(layer);
 		Sprite::Create(ent, Texture::Load("imgs/dreg-sheet.png", 216, 24, 24, 24, 0, 1));
 		Animation::Add(ent, "idle", "0x63 1-3x5", true);
 		Animation::Add(ent, "move", "4-8x4", false);
@@ -44,6 +45,7 @@ Character * Character::Add(State* stt, std::string name, int x, int y, unsigned 
 
 	else if (name == "Mosh")
 	{
+		auto ent = stt->Add_Entity<Character>(layer);
 		Sprite::Create(ent, Texture::Load("imgs/mosh-sheet.png", 168, 24, 24, 24, 0, 1));
 		Animation::Add(ent, "idle", "0x70 1x3", true);
 		Animation::Add(ent, "move", "0x6 2-6x6", false);
@@ -53,6 +55,7 @@ Character * Character::Add(State* stt, std::string name, int x, int y, unsigned 
 
 	else if (name == "Raiden")
 	{
+		auto ent = stt->Add_Entity<Character>(layer);
 		Sprite::Create(ent, Texture::Load("imgs/raiden-sheet.png", 96, 24, 24, 24, 0, 1));
 		Animation::Add(ent, "idle", "0", true);
 		Animation::Add(ent, "move", "0-4x4", false);
@@ -62,6 +65,7 @@ Character * Character::Add(State* stt, std::string name, int x, int y, unsigned 
 
 	else if (name == "Tar")
 	{
+		auto ent = stt->Add_Entity<Character>(layer);
 		Sprite::Create(ent, Texture::Load("imgs/tar-sheet.png", 216, 24, 24, 24, 0, 1));
 		Animation::Add(ent, "idle", "0x85 1x5", true);
 		Animation::Add(ent, "move", "0x4 2-8x4", false);
@@ -71,7 +75,10 @@ Character * Character::Add(State* stt, std::string name, int x, int y, unsigned 
 
 	else if (name == "Benio")
 	{
+		auto ent = stt->Add_Entity<Character>(layer);
 		Sprite::Create(ent, Texture::Load("imgs/benio.png", 24, 24, 24, 24, 0, 1));
+		Animation::Add(ent, "idle", "0", true);
+		Animation::Add(ent, "move", "0", false);
 		Movement::Set(ent, 2.3, 10);
 		return ent;
 	}
